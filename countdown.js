501dfe157d94a59ba8e66f8f65c6e18e3fc7caa8
gonzalezenios <gonzalezeniood@gmail.com>
2020-06-24 20:58:39 -0300

    usando modulos de npm

---
+++ countdown.js
@@ -0,0 +1,25 @@
+const parseArgs =  require ("minimist")
+
+//
+//  Collect the time from agrv
+//
+
+const { time } = parseArgs(process.argv)
+
+if (!time) {
+    throw new Error ("--time is required")
+}
+
+if (!parseInt(time)) {
+    throw new Error ("--time must be a number")
+}
+
+console.log(time);
+
+//
+//  Print stars in-line
+//
+
+//
+// Run a timer and remove stars
+//
