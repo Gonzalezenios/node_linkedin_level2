1bc7f9b041af0dc492726b43d459eed6d316b7d2
gonzalezenios <gonzalezeniood@gmail.com>
2020-06-24 21:33:44 -0300

    usando sing line log

--- countdown.js
+++ countdown.js
@@ -1,10 +1,10 @@
 const parseArgs =  require ("minimist")
+const { stdout:log } = require ("single-line-log")
+const { time } = parseArgs(process.argv)
 
 //
 //  Collect the time from agrv
 //
-
-const { time } = parseArgs(process.argv)
 
 if (!time) {
     throw new Error ("--time is required")
@@ -14,7 +14,18 @@ if (!parseInt(time)) {
     throw new Error ("--time must be a number")
 }
 
-console.log(time);
+const count = parseInt(time)
+let message = "";
+
+for (let i=0; i<count; i++) {
+    message += "*";
+}
+
+log (message);
+
+setTimeout( () => {
+    log("overwrites line");
+}, 2000)
 
 //
 //  Print stars in-line
