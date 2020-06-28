fbd7aa96ee446f074b00ebf3b4cdb1da67291113
gonzalezenios <gonzalezeniood@gmail.com>
2020-06-27 23:36:46 -0300

    usando web socket io client & server

--- socket-client.js
+++ socket-client.js
@@ -6,3 +6,10 @@ socket.on("connect", () => {
     console.log("socket.io client connected");
 });
 
+socket.on("chat-message", (message, id) => {
+    console.log(`${id}: ${message}`);
+});
+
+process.stdin.on("data", data => {
+    socket.emit("chat", data.toString().trim());
+}); 
--- socket-server.js
+++ socket-server.js
@@ -4,8 +4,13 @@ const socketIO = require ("socket.io");
 const server = createServer().listen(3000);
 const io = socketIO(server);
 
-io.compress("connection", socket => {
+io.on("connection", socket => {
     console.log(`${io.engine.clientsCount} connections`);
+
+    socket.on("chat", message => {
+        console.log(`${socket.id}: ${message}`);
+        io.sockets.emit("chat-message", message, socket.id);
+    });
 
     socket.on("disconnect", () => {
         console.log (`disconnect: ${socket.id}`);
